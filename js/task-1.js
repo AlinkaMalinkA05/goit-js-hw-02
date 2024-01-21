cbd0cbff39b91381c5a56f2c8ec969fed7e52ad2
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-01-21 20:26:37 +0100

    added 4 filed tasks1-4

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,10 @@
-function makeTransaction(quantity,pricePerDroid ) { return`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+    const totalPrice = (quantity * ricePerDroid)
+    if (orderAmount > customerCredits)
+    { return "Insufficient funds!" }
+    else {
+        return `"You ordered ${quantity} droids worth ${totalPrice} credits!"`
+    } 
+    console.log(makeTransaction(5, 3000, 23000));
+    }
 
-}
-
-console.log(makeTransaction(5, 3000));
-console.log(makeTransaction(3, 1000));
-console.log(makeTransaction(10, 500));
