d0400e6487b5261432a00ebf4e8a2e4e1f20f89e
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-01-21 22:21:20 +0100

    task 4 is ready

--- js/task-3.js
+++ js/task-3.js
@@ -11,3 +11,17 @@ console.log(checkForSpam("Trust me, this is not a spam message"));
 console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
 console.log(checkForSpam("[SPAM] How to earn fast money?")); 
 
+function getShippingCost(country) {
+    switch (country) { 
+        case "China":
+            return `Shipping to ${country} will cost <100> credits`;
+        case "Chile":
+            return `Shipping to ${country} will cost <250> credits`;
+        case "Australia ":
+            return `Shipping to ${country} will cost <170> credits`;
+        case "Jamaica ":
+            return `Shipping to ${country} will cost <120> credits`;
+        default:
+            return "Sorry, there is no delivery to your country"  
+    }
+        }
