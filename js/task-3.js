a6cb76904171b675c2a8a9f5bee216f8ef4c9130
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-01-22 20:40:50 +0100

    changed mistakes

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,6 @@
 function checkForSpam(message) {
     const normalizedMessage = message.toLowerCase();
-return normalizedMessage.includes("spam") || normalizedMessage.includes("sale")
+    return normalizedMessage.includes("spam") || normalizedMessage.includes("sale");
 } 
 
 console.log(checkForSpam("Latest technology news"));
